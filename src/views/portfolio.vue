1adbc732dfddb887fb7b791ab290f0a8bf7c56b4
Masood <masoodsse@gmail.com>
2023-12-22 12:17:52 +0500

    added pictures for portfolio

--- src/views/portfolio.vue
+++ src/views/portfolio.vue
@@ -1,16 +1,32 @@
 <template>
-  <div id="portfolio" class="w-full h-screen pt-40 flex justify-between">
-    <div class="w-1/4 ml-10">
+  <div id="portfolio" class="w-full h-screen pt-40 flex justify-between relative ">
+    <div class="absolute left-[10%] top-30">
       <img src="../../public/portfolio-left-dec.png" alt="" class="w-full" />
     </div>
-    <div class="w-3/4 mr-52 ">
-      <h1 class="text-5xl text-center font-extrabold text-gray-900">
+    <div class="w-full relative">
+      <h1 class="text-5xl text-center font-extrabold text-gray-900 leading-normal">
         Our Recent <span class="text-sky-500">Projects</span> & Case<br />
         Studies <span class="text-red-500">For Clients</span>
       </h1>
       <h2 class="text-center text-4xl font-extrabold text-red-200">OUR PORTFOLIO</h2>
+    <div class="w-11/12 flex justify-between items-center absolute  left-28 top-52">
+        <img src="../../public/portfolio-01.jpg" alt="" class="mr-10 rounded-xl ">
+        <img src="../../public/portfolio-02.jpg" alt="" class="mr-10 rounded-xl">
+        <img src="../../public/portfolio-03.jpg" alt="" class="mr-10 rounded-xl">
+         <img src="../../public/portfolio-04.jpg" alt="" class="mr-10 rounded-xl">
+
     </div>
+    </div>
+
   </div>
+  <div class="w-full relative">
+      <h1 class="text-5xl text-center font-bold text-gray-900 leading-normal">
+        Select A Suitable  <span class="text-sky-500">Plan</span> for Your<br />
+        Next <span class="text-red-500">Projects</span>
+      </h1>
+      <h2 class="text-center text-4xl font-bold text-red-200">OUR PLANS</h2>
+    </div>
+    
 </template>
 
 <script setup></script>
